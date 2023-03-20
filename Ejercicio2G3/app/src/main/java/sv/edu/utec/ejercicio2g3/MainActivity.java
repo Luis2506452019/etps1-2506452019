cd09790a829af277336b4a13ff92f0509d133630
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-03-20 15:52:53 -0600

    Summary

---
+++ Ejercicio2G3/app/src/main/java/sv/edu/utec/ejercicio2g3/MainActivity.java
@@ -0,0 +1,45 @@
+package sv.edu.utec.ejercicio2g3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    EditText numero;
+    TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        numero = findViewById(R.id.edtIngresar);
+        resultado = findViewById(R.id.tvResultado);
+    }
+
+    public void Calcular(View v) {
+        //Variables
+        String mensaje = "";
+        int res = 0;
+        res = Integer.parseInt(numero.getText().toString());
+
+        //Condiciones
+        if (res < 10) {
+            mensaje = "\"Tiene un dígito\"";
+        } else if (res < 100) {
+            mensaje = "\"Tiene dos dígitos\"";
+        } else if (res < 1000) {
+            mensaje = "\"Tiene tres dígitos\"";
+        } else {
+            mensaje = "\"Error en la entrada de datos.\"";
+        }
+
+        //Texto a mostrar en Textview
+        resultado.setText(mensaje);
+    }
+}
