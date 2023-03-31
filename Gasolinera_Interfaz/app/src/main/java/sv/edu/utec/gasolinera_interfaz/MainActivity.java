1f5548711452924c5666f3560f3291f6d9fd4a82
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-03-30 21:58:09 -0600

    Summary

--- Gasolinera_Interfaz/app/src/main/java/sv/edu/utec/gasolinera_interfaz/MainActivity.java
+++ Gasolinera_Interfaz/app/src/main/java/sv/edu/utec/gasolinera_interfaz/MainActivity.java
@@ -2,19 +2,27 @@ package sv.edu.utec.gasolinera_interfaz;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import sv.edu.utec.gasolinera_interfaz.clases.EstacionFragment;
+import sv.edu.utec.gasolinera_interfaz.clases.HorarioFragment;
+import sv.edu.utec.gasolinera_interfaz.clases.InicioFragment;
+import sv.edu.utec.gasolinera_interfaz.clases.OfertaFragment;
+import sv.edu.utec.gasolinera_interfaz.clases.UbicacionFragment;
+
 public class MainActivity extends AppCompatActivity {
     //Var Pantallas
-    LinearLayout lin_inicio, lin_estacion, lin_ubicacion, lin_horario, lin_oferta;
+    FragmentTransaction transaction;
+    Fragment fragment_inicio, fragment_estacion, fragment_ubicacion, fragment_horario, fragment_oferta;
 
     //Var Circulo
     LinearLayout cir_inicio, cir_estacion, cir_ubicacion, cir_horario, cir_oferta;
@@ -22,17 +30,19 @@ public class MainActivity extends AppCompatActivity {
     //Var Menu
     NavigationView navigationView;
     LinearLayout lin_menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Pantallas
-        lin_inicio = (LinearLayout) findViewById(R.id.pantallaInicio);
-        lin_estacion = (LinearLayout) findViewById(R.id.pantallaEstacion);
-        lin_ubicacion = (LinearLayout) findViewById(R.id.pantallaUbicacion);
-        lin_horario = (LinearLayout) findViewById(R.id.pantallaHorario);
-        lin_oferta = (LinearLayout) findViewById(R.id.pantallaOferta);
+        fragment_inicio = new InicioFragment();
+        fragment_estacion = new EstacionFragment();
+        fragment_ubicacion = new UbicacionFragment();
+        fragment_horario = new HorarioFragment();
+        fragment_oferta = new OfertaFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragments, fragment_inicio).commit();
 
         //Circulos
         cir_inicio = (LinearLayout) findViewById(R.id.circuloInicio);
@@ -45,176 +55,166 @@ public class MainActivity extends AppCompatActivity {
         lin_menu = findViewById(R.id.lin_volver);
         navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                transaction=getSupportFragmentManager().beginTransaction();
                 switch (item.getItemId()){
                     case R.id.inicio_item:
                     {
-                        mostrarInicio();
+                        transaction.replace(R.id.contenedorFragments,fragment_inicio);
+                        transaction.addToBackStack(null);
+                        mostrarCirInicio();
                         break;
                     }
                     case R.id.estacion_item:
                     {
-                        mostrarEstacion();
+                        transaction.replace(R.id.contenedorFragments,fragment_estacion);
+                        transaction.addToBackStack(null);
+                        mostrarCirEstacion();
                         break;
                     }
                     case R.id.ubicacion_item:
                     {
-                        mostrarUbicacion();
+                        transaction.replace(R.id.contenedorFragments,fragment_ubicacion);
+                        transaction.addToBackStack(null);
+                        mostrarCirUbicacion();
                         break;
                     }
                     case R.id.horario_item:
                     {
-                        mostrarHorario();
+                        transaction.replace(R.id.contenedorFragments,fragment_horario);
+                        transaction.addToBackStack(null);
+                        mostrarCirHorario();
                         break;
                     }
                     case R.id.oferta_item:
                     {
-                        mostrarOferta();
+                        transaction.replace(R.id.contenedorFragments,fragment_oferta);
+                        transaction.addToBackStack(null);
+                        mostrarCirOferta();
                         break;
                     }
                 }
+                transaction.commit();
                 return false;
             }
         });
-
     }
 
-    //Metodo para ir cambiando entre pantallas
+    //Metodo para ir cambiando entre pantallas menu abajo
     public void mostrarDatos(View view){
+        transaction=getSupportFragmentManager().beginTransaction();
         switch (view.getId()){
             case R.id.LayoutInicio:
             {
-                mostrarInicio();
+                transaction.replace(R.id.contenedorFragments,fragment_inicio);
+                transaction.addToBackStack(null);
+                mostrarCirInicio();
                 break;
             }
             case R.id.LayoutEstacion:
             {
-                mostrarEstacion();
+                transaction.replace(R.id.contenedorFragments,fragment_estacion);
+                transaction.addToBackStack(null);
+                mostrarCirEstacion();
                 break;
             }
             case R.id.LayoutUbicacion:
             {
-                mostrarUbicacion();
+                transaction.replace(R.id.contenedorFragments,fragment_ubicacion);
+                transaction.addToBackStack(null);
+                mostrarCirUbicacion();
                 break;
             }
             case R.id.LayoutHorario:
             {
-                mostrarHorario();
+                transaction.replace(R.id.contenedorFragments,fragment_horario);
+                transaction.addToBackStack(null);
+                mostrarCirHorario();
                 break;
             }
             case R.id.LayoutOferta:
             {
-                mostrarOferta();
+                transaction.replace(R.id.contenedorFragments,fragment_oferta);
+                transaction.addToBackStack(null);
+                mostrarCirOferta();
                 break;
             }
         }
+        transaction.commit();
     }
-    public void mostrarInicio() {
+
+    public void mostrarCirInicio() {
         //Inicio
-        lin_inicio.setVisibility(View.VISIBLE);
         cir_inicio.setVisibility(View.VISIBLE);
 
         //Opciones
-        lin_estacion.setVisibility(View.GONE);
         cir_estacion.setVisibility(View.INVISIBLE);
-
-        lin_ubicacion.setVisibility(View.GONE);
         cir_ubicacion.setVisibility(View.INVISIBLE);
-
-        lin_horario.setVisibility(View.GONE);
         cir_horario.setVisibility(View.INVISIBLE);
-
-        lin_oferta.setVisibility(View.GONE);
         cir_oferta.setVisibility(View.INVISIBLE);
 
+        //OcultarMenu
         navigationView.setVisibility(View.INVISIBLE);
         lin_menu.setVisibility(View.INVISIBLE);
     }
-    public void mostrarEstacion() {
+    public void mostrarCirEstacion() {
         //Estacion
-        lin_estacion.setVisibility(View.VISIBLE);
         cir_estacion.setVisibility(View.VISIBLE);
 
         //Opciones
-        lin_inicio.setVisibility(View.GONE);
         cir_inicio.setVisibility(View.INVISIBLE);
-
-        lin_ubicacion.setVisibility(View.GONE);
         cir_ubicacion.setVisibility(View.INVISIBLE);
-
-        lin_horario.setVisibility(View.GONE);
         cir_horario.setVisibility(View.INVISIBLE);
-
-        lin_oferta.setVisibility(View.GONE);
         cir_oferta.setVisibility(View.INVISIBLE);
 
+        //OcultarMenu
         navigationView.setVisibility(View.INVISIBLE);
         lin_menu.setVisibility(View.INVISIBLE);
     }
 
-    public void mostrarUbicacion() {
+    public void mostrarCirUbicacion() {
         //Ubicacion
-        lin_ubicacion.setVisibility(View.VISIBLE);
         cir_ubicacion.setVisibility(View.VISIBLE);
 
         //Opciones
-        lin_inicio.setVisibility(View.GONE);
         cir_inicio.setVisibility(View.INVISIBLE);
-
-        lin_estacion.setVisibility(View.GONE);
         cir_estacion.setVisibility(View.INVISIBLE);
-
-        lin_horario.setVisibility(View.GONE);
         cir_horario.setVisibility(View.INVISIBLE);
-
-        lin_oferta.setVisibility(View.GONE);
         cir_oferta.setVisibility(View.INVISIBLE);
 
+        //OcultarMenu
         navigationView.setVisibility(View.INVISIBLE);
         lin_menu.setVisibility(View.INVISIBLE);
     }
 
-    public void mostrarHorario() {
+    public void mostrarCirHorario() {
         //Horario
-        lin_horario.setVisibility(View.VISIBLE);
         cir_horario.setVisibility(View.VISIBLE);
 
         //Opciones
-        lin_inicio.setVisibility(View.GONE);
         cir_inicio.setVisibility(View.INVISIBLE);
-
-        lin_estacion.setVisibility(View.GONE);
         cir_estacion.setVisibility(View.INVISIBLE);
-
-        lin_ubicacion.setVisibility(View.GONE);
         cir_ubicacion.setVisibility(View.INVISIBLE);
-
-        lin_oferta.setVisibility(View.GONE);
         cir_oferta.setVisibility(View.INVISIBLE);
 
+        //OcultarMenu
         navigationView.setVisibility(View.INVISIBLE);
         lin_menu.setVisibility(View.INVISIBLE);
     }
 
-    public void mostrarOferta() {
+    public void mostrarCirOferta() {
         //Oferta
-        lin_oferta.setVisibility(View.VISIBLE);
         cir_oferta.setVisibility(View.VISIBLE);
 
         //Opciones
-        lin_inicio.setVisibility(View.GONE);
         cir_inicio.setVisibility(View.INVISIBLE);
-
-        lin_estacion.setVisibility(View.GONE);
         cir_estacion.setVisibility(View.INVISIBLE);
-
-        lin_ubicacion.setVisibility(View.GONE);
         cir_ubicacion.setVisibility(View.INVISIBLE);
-
-        lin_horario.setVisibility(View.GONE);
         cir_horario.setVisibility(View.INVISIBLE);
 
+        //OcultarMenu
         navigationView.setVisibility(View.INVISIBLE);
         lin_menu.setVisibility(View.INVISIBLE);
     }
