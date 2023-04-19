7751e60eaffd19ae9477285013c873521819daf7
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-04-19 12:44:14 -0600

    Summary

---
+++ Gasolinera_Interfaz/Gasolinera_Interfaz v2.0.0/app/src/main/java/sv/edu/utec/gasolinera_interfaz/MainActivity.java
@@ -0,0 +1,85 @@
+package sv.edu.utec.gasolinera_interfaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private AppBarConfiguration mAppBarConfiguration;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.inicioFragment, R.id.estacionFragment, R.id.ubicacionFragment, R.id.horarioFragment, R.id.ofertaFragment)
+                .setOpenableLayout(drawer).build();
+
+        NavController navController = Navigation.findNavController(this, R.id.contenedorFragments);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        BottomNavigationView bottom_nav_view = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottom_nav_view, navController);
+    }
+
+    //Creacion de menu derecha superior
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_main, menu);
+        menuIconColor(menu, R.color.yellow);
+        return true;
+    }
+
+    //Color Icono menu derecha superior
+    public void menuIconColor(Menu menu, int color){
+        for(int i=0; i<menu.size(); i++){
+            Drawable drawable = menu.getItem(i).getIcon();
+            if(drawable != null){
+                drawable.mutate();
+                drawable.setColorFilter(getResources().getColor(color), PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+    }
+
+    //Eventos menu derecha superior
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.userFragment) {
+            Toast.makeText(this, "Se presionó el ícono de usuario", Toast.LENGTH_LONG).show();
+            return true;
+        }     return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.contenedorFragments);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
+
