8dec4a12be7c329ff2199f259996ea893e95153f
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-04-19 21:56:15 -0600

    Summary

--- Parcial3_2506452019/app/src/main/java/sv/edu/utec/parcial3_2506452019/MainActivity.java
+++ Parcial3_2506452019/app/src/main/java/sv/edu/utec/parcial3_2506452019/MainActivity.java
@@ -1,16 +1,24 @@
 package sv.edu.utec.parcial3_2506452019;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import sv.edu.utec.parcial3_2506452019.clases.BuscarFragment;
+import sv.edu.utec.parcial3_2506452019.clases.InicioFragment;
+
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +27,34 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
-        navView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.transparent));
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.transparent));
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.inicio:
+                    fragment = new InicioFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.buscar:
+                    fragment = new BuscarFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        };
+    };
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.contenedorFragments,fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
---
+++ Parcial3_2506452019/app/src/main/java/sv/edu/utec/parcial3_2506452019/clases/InicioFragment.java
@@ -0,0 +1,23 @@
+package sv.edu.utec.parcial3_2506452019.clases;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sv.edu.utec.parcial3_2506452019.R;
+
+public class InicioFragment extends Fragment {
+    public InicioFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
+}
