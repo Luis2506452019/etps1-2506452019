48df79a929c5d801b69d7b4ce92bd4fc3d0d33d7
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-04-27 15:35:09 -0600

    Summary

--- Gasolinera_Interfaz/Gasolinera_Interfaz v3.0.0/app/src/main/java/sv/edu/utec/gasolinera_interfaz/clases/OfertaFragment.java
+++ Gasolinera_Interfaz/Gasolinera_Interfaz v3.0.0/app/src/main/java/sv/edu/utec/gasolinera_interfaz/clases/OfertaFragment.java
@@ -7,9 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import sv.edu.utec.gasolinera_interfaz.R;
 public class OfertaFragment extends Fragment {
+
+    Button btnRegistrar;
+    ImageView imgPuma, imgTexaco;
 
     public OfertaFragment() {
         // Required empty public constructor
@@ -18,6 +24,33 @@ public class OfertaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_oferta, container, false);
+        View vista = inflater.inflate(R.layout.fragment_oferta, container, false);
+
+        btnRegistrar = (Button) vista.findViewById(R.id.btnRegistraCupon);
+        imgPuma = (ImageView) vista.findViewById(R.id.imagenIconPuma);
+        imgTexaco = (ImageView) vista.findViewById(R.id.imagenIconTexaco);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "Registrar Cupón", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgPuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "Gasolinera Puma", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imgTexaco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "Gasolinera Texaco", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return vista;
     }
 }
