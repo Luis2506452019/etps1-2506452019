116798b1496ca2dc267fe53a08772dd181d48b21
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-05-06 15:40:03 -0600

    Summary

--- Gasolinera_Interfaz/Gasolinera_Interfaz v3.0.0/app/src/main/java/sv/edu/utec/gasolinera_interfaz/clases/HorarioFragment.java
+++ Gasolinera_Interfaz/Gasolinera_Interfaz v3.0.0/app/src/main/java/sv/edu/utec/gasolinera_interfaz/clases/HorarioFragment.java
@@ -28,79 +28,32 @@ public class HorarioFragment extends Fragment {
         // Inflate the layout for this fragment
         View vista = inflater.inflate(R.layout.fragment_horario, container, false);
 
+        // Variables Spinner (ComboBox)
         Spinner spin1 = (Spinner) vista.findViewById(R.id.spinner1);
         Spinner spin2 = (Spinner) vista.findViewById(R.id.spinner2);
         Spinner spin3 = (Spinner) vista.findViewById(R.id.spinner3);
         Spinner spin4 = (Spinner) vista.findViewById(R.id.spinner4);
         Spinner spin5 = (Spinner) vista.findViewById(R.id.spinner5);
 
+        // Personalizacion del Spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.gasolinera_array, R.layout.elemento_spinner_personalizado);
         adapter.setDropDownViewResource( R.layout.elemento_lista_spinner_personalizado);
+
+        // Llenando Spinner
         spin1.setAdapter(adapter);
         spin2.setAdapter(adapter);
         spin3.setAdapter(adapter);
         spin4.setAdapter(adapter);
         spin5.setAdapter(adapter);
 
+        // Evento para Opciones con Toast
         spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = (String) spin1.getItemAtPosition(position);
-                Toast.makeText(getActivity(), selected, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-        spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = (String) spin2.getItemAtPosition(position);
-                Toast.makeText(getActivity(), selected, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-        spin3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = (String) spin3.getItemAtPosition(position);
-                Toast.makeText(getActivity(), selected, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-        spin4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = (String) spin4.getItemAtPosition(position);
-                Toast.makeText(getActivity(), selected, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-        spin5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selected = (String) spin5.getItemAtPosition(position);
-                Toast.makeText(getActivity(), selected, Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                Toast.makeText(getActivity(), parent.getSelectedItem().toString(),
+                        Toast.LENGTH_SHORT).show();
             }
 
             @Override
