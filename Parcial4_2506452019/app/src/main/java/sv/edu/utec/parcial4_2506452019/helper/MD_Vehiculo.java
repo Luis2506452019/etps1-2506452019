ec4385903880b45f5c9425762b68b0edcc681cda
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-05-17 21:50:08 -0600

    Summary

--- Parcial4_2506452019/app/src/main/java/sv/edu/utec/parcial4_2506452019/helper/MD_Vehiculo.java
+++ Parcial4_2506452019/app/src/main/java/sv/edu/utec/parcial4_2506452019/helper/MD_Vehiculo.java
@@ -1,11 +1,36 @@
 package sv.edu.utec.parcial4_2506452019.helper;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import androidx.annotation.Nullable;
 
 public class MD_Vehiculo extends BaseHelper{
+    Context context;
+
     public MD_Vehiculo(@Nullable Context context) {
         super(context);
+        this.context = context;
+    }
+
+    public long insertVehiculo(String sMarca, String sModelo){
+        long codigo = 0;
+
+        try {
+            BaseHelper baseHelp = new BaseHelper(context);
+            SQLiteDatabase bd = baseHelp.getWritableDatabase();
+
+            ContentValues vVehiculo = new ContentValues();
+            vVehiculo.put("sMarca", sMarca);
+            vVehiculo.put("sModelo", sModelo);
+
+            codigo= bd.insert(NOMBRE_TABLAcl, null, vVehiculo);
+            return codigo;
+        }
+        catch (Exception ex){
+            ex.toString();
+            return codigo=0;
+        }
     }
 }
